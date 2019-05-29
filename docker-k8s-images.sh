0dc7d98cd975e40a99c3e73b5757b5df9c0bcd1c
abelsu7 <abel1995@163.com>
2019-05-29 14:50:17 +0800

    Add docker-k8s-images.sh

---
+++ docker-k8s-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+images=(
+    kube-apiserver:v1.13.3
+    kube-controller-manager:v1.13.3
+    kube-scheduler:v1.13.3
+    kube-proxy:v1.13.3
+    pause:3.1
+    etcd:3.2.24
+    coredns:1.2.6
+)
+
+for imageName in ${images[@]} ; do
+    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/${imageName}
+    docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/${imageName} k8s.gcr.io/${imageName}
+    docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/${imageName}
+done
+
+docker images
